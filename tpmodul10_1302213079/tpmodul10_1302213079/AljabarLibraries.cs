370235177e2a803f9ebabd64768c912c0bcafa99
medavidrahmadana <me.davidrahmadana@gmail.com>
2023-05-02 07:07:21 +0700

    Create Project dan Create Libraries
    Lupa di commit dari awal

---
+++ tpmodul10_1302213079/tpmodul10_1302213079/AljabarLibraries.cs
@@ -0,0 +1,37 @@
+﻿namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+            double d = b * b - 4 * a * c;
+            if (d < 0)
+            {
+                return new double[] { double.NaN, double.NaN };
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                return new double[] { x1, x2 };
+            }
+        }
+        public static double[] hasilKuadrat(double[] persamaan)
+        {
+            if (persamaan.Length != 2 || persamaan.Length == null)
+            {
+                throw new ArgumentException("Persamaan harus berisi dua angka.");
+            }
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double rumus = a * a;
+            double aSquare = 2 * a * b;
+            double bSquare = b * b;
+
+            return new double[] { rumus, aSquare, bSquare };
+        }
+    }
+}
---
+++ tpmodul10_1302213079/tpmodul10_1302213079/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AljabarLibraries;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double[] persamaan1 = new double[] { 1, -3, -10 };
+        double[] test = Aljabar.AkarPersamaanKuadrat(persamaan1);
+        Console.WriteLine("Berikut hasil akar persamaan kuadrat: ");
+        Console.Write("A. {" + test[0]);
+        Console.WriteLine("," + test[1]+"}");
+        double[] persamaan2 = { 2, -3 };
+        double[] test2 = Aljabar.hasilKuadrat(persamaan2);
+        Console.Write("B. {" + test2[0]);
+        Console.Write("," + test2[1]);
+        Console.Write(" " + test2[2] + "}");
+        Console.WriteLine(" ");
+    }
+}
